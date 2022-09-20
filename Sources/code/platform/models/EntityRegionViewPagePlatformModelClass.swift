0a976fef4f6f474445ac47bb5abdacb30222a74f
Jigar Dafda <jigar.dafda@gmail.com>
2022-09-20 13:27:19 +0000

    [Auto Generated] 0.1.26

---
+++ Sources/code/platform/models/EntityRegionViewPagePlatformModelClass.swift
@@ -0,0 +1,73 @@
+
+
+import Foundation
+public extension PlatformClient {
+    /*
+         Model: EntityRegionViewPage
+         Used By: Serviceability
+     */
+
+    class EntityRegionViewPage: Codable {
+        public var size: Int
+
+        public var type: String
+
+        public var hasNext: Bool
+
+        public var itemTotal: Int
+
+        public var current: Int
+
+        public enum CodingKeys: String, CodingKey {
+            case size
+
+            case type
+
+            case hasNext = "has_next"
+
+            case itemTotal = "item_total"
+
+            case current
+        }
+
+        public init(current: Int, hasNext: Bool, itemTotal: Int, size: Int, type: String) {
+            self.size = size
+
+            self.type = type
+
+            self.hasNext = hasNext
+
+            self.itemTotal = itemTotal
+
+            self.current = current
+        }
+
+        required public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+
+            size = try container.decode(Int.self, forKey: .size)
+
+            type = try container.decode(String.self, forKey: .type)
+
+            hasNext = try container.decode(Bool.self, forKey: .hasNext)
+
+            itemTotal = try container.decode(Int.self, forKey: .itemTotal)
+
+            current = try container.decode(Int.self, forKey: .current)
+        }
+
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+
+            try? container.encodeIfPresent(size, forKey: .size)
+
+            try? container.encodeIfPresent(type, forKey: .type)
+
+            try? container.encodeIfPresent(hasNext, forKey: .hasNext)
+
+            try? container.encodeIfPresent(itemTotal, forKey: .itemTotal)
+
+            try? container.encodeIfPresent(current, forKey: .current)
+        }
+    }
+}
---
+++ Sources/code/platform/models/ListViewItemsPlatformModelClass.swift
@@ -0,0 +1,113 @@
+
+
+import Foundation
+public extension PlatformClient {
+    /*
+         Model: ListViewItems
+         Used By: Serviceability
+     */
+
+    class ListViewItems: Codable {
+        public var channels: ListViewChannels
+
+        public var pincodesCount: Int
+
+        public var name: String
+
+        public var isActive: Bool
+
+        public var storesCount: Int
+
+        public var companyId: Int
+
+        public var slug: String
+
+        public var product: ListViewProduct
+
+        public var zoneId: String
+
+        public enum CodingKeys: String, CodingKey {
+            case channels
+
+            case pincodesCount = "pincodes_count"
+
+            case name
+
+            case isActive = "is_active"
+
+            case storesCount = "stores_count"
+
+            case companyId = "company_id"
+
+            case slug
+
+            case product
+
+            case zoneId = "zone_id"
+        }
+
+        public init(channels: ListViewChannels, companyId: Int, isActive: Bool, name: String, pincodesCount: Int, product: ListViewProduct, slug: String, storesCount: Int, zoneId: String) {
+            self.channels = channels
+
+            self.pincodesCount = pincodesCount
+
+            self.name = name
+
+            self.isActive = isActive
+
+            self.storesCount = storesCount
+
+            self.companyId = companyId
+
+            self.slug = slug
+
+            self.product = product
+
+            self.zoneId = zoneId
+        }
+
+        required public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+
+            channels = try container.decode(ListViewChannels.self, forKey: .channels)
+
+            pincodesCount = try container.decode(Int.self, forKey: .pincodesCount)
+
+            name = try container.decode(String.self, forKey: .name)
+
+            isActive = try container.decode(Bool.self, forKey: .isActive)
+
+            storesCount = try container.decode(Int.self, forKey: .storesCount)
+
+            companyId = try container.decode(Int.self, forKey: .companyId)
+
+            slug = try container.decode(String.self, forKey: .slug)
+
+            product = try container.decode(ListViewProduct.self, forKey: .product)
+
+            zoneId = try container.decode(String.self, forKey: .zoneId)
+        }
+
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+
+            try? container.encodeIfPresent(channels, forKey: .channels)
+
+            try? container.encodeIfPresent(pincodesCount, forKey: .pincodesCount)
+
+            try? container.encodeIfPresent(name, forKey: .name)
+
+            try? container.encodeIfPresent(isActive, forKey: .isActive)
+
+            try? container.encodeIfPresent(storesCount, forKey: .storesCount)
+
+            try? container.encodeIfPresent(companyId, forKey: .companyId)
+
+            try? container.encodeIfPresent(slug, forKey: .slug)
+
+            try? container.encodeIfPresent(product, forKey: .product)
+
+            try? container.encodeIfPresent(zoneId, forKey: .zoneId)
+        }
+    }
+}
