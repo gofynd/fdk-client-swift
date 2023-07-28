82cd5224e084b6f029417fc1d7adb8aab28e9f4c
Jigar Dafda <jigar.dafda@gmail.com>
2023-07-28 12:59:28 +0000

    [Auto Generated] 1.1.2

---
+++ Sources/code/platform/Models/Serviceability/EntityRegionView_ResponseServiceabilityPlatformModel.swift
@@ -0,0 +1,125 @@
+
+
+import Foundation
+
+public extension PlatformClient.Serviceability {
+    /*
+         Model: EntityRegionView_Response
+         Used By: Serviceability
+     */
+
+    class EntityRegionView_Response: Codable {
+        public var error: EntityRegionView_Error
+
+        public var page: EntityRegionView_page
+
+        public var data: [EntityRegionView_Items]
+
+        public var success: Bool
+
+        public enum CodingKeys: String, CodingKey {
+            case error
+
+            case page
+
+            case data
+
+            case success
+        }
+
+        public init(data: [EntityRegionView_Items], error: EntityRegionView_Error, page: EntityRegionView_page, success: Bool) {
+            self.error = error
+
+            self.page = page
+
+            self.data = data
+
+            self.success = success
+        }
+
+        required public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+
+            error = try container.decode(EntityRegionView_Error.self, forKey: .error)
+
+            page = try container.decode(EntityRegionView_page.self, forKey: .page)
+
+            data = try container.decode([EntityRegionView_Items].self, forKey: .data)
+
+            success = try container.decode(Bool.self, forKey: .success)
+        }
+
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+
+            try? container.encodeIfPresent(error, forKey: .error)
+
+            try? container.encodeIfPresent(page, forKey: .page)
+
+            try? container.encodeIfPresent(data, forKey: .data)
+
+            try? container.encodeIfPresent(success, forKey: .success)
+        }
+    }
+}
+
+public extension PlatformClient.ApplicationClient.Serviceability {
+    /*
+         Model: EntityRegionView_Response
+         Used By: Serviceability
+     */
+
+    class EntityRegionView_Response: Codable {
+        public var error: EntityRegionView_Error
+
+        public var page: EntityRegionView_page
+
+        public var data: [EntityRegionView_Items]
+
+        public var success: Bool
+
+        public enum CodingKeys: String, CodingKey {
+            case error
+
+            case page
+
+            case data
+
+            case success
+        }
+
+        public init(data: [EntityRegionView_Items], error: EntityRegionView_Error, page: EntityRegionView_page, success: Bool) {
+            self.error = error
+
+            self.page = page
+
+            self.data = data
+
+            self.success = success
+        }
+
+        required public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+
+            error = try container.decode(EntityRegionView_Error.self, forKey: .error)
+
+            page = try container.decode(EntityRegionView_page.self, forKey: .page)
+
+            data = try container.decode([EntityRegionView_Items].self, forKey: .data)
+
+            success = try container.decode(Bool.self, forKey: .success)
+        }
+
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+
+            try? container.encodeIfPresent(error, forKey: .error)
+
+            try? container.encodeIfPresent(page, forKey: .page)
+
+            try? container.encodeIfPresent(data, forKey: .data)
+
+            try? container.encodeIfPresent(success, forKey: .success)
+        }
+    }
+}
