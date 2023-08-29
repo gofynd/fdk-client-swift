cf81c72ccc3e16e3701e0dd9e898ed5cc0bad3ce
Jigar Dafda <jigar.dafda@gmail.com>
2023-08-29 08:56:01 +0000

    [Auto Generated] 1.3.0

--- Sources/code/application/ApplicationAPIClient.swift
+++ Sources/code/application/ApplicationAPIClient.swift
@@ -13,7 +13,7 @@ class ApplicationAPIClient {
         var headers = [
             (key: "Authorization", value: "Bearer " + "\(config.applicationId):\(config.applicationToken)".asBase64)
         ]
-        headers.append((key: "x-fp-sdk-version", value: "2.0.0"))
+        headers.append((key: "x-fp-sdk-version", value: "1.3.0"))
         headers.append(contentsOf: extraHeaders)
         headers.append(contentsOf: config.extraHeaders)
         if let userAgent = config.userAgent {
