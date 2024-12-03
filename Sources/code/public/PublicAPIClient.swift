38c99ab647e27da35f16a4d81141f6e213c06a70
Jigar Dafda <jigar.dafda@gmail.com>
2024-12-03 09:17:34 +0000

    [Auto Generated] 1.5.2

--- Sources/code/public/PublicAPIClient.swift
+++ Sources/code/public/PublicAPIClient.swift
@@ -9,7 +9,7 @@ class PublicAPIClient {
                         responseType: String = "application/json",
                         onResponse: @escaping OnResponse) {
         var headers = [
-            (key: "x-fp-sdk-version", value: "1.5.1")
+            (key: "x-fp-sdk-version", value: "1.5.2")
         ]
         headers.append(contentsOf: extraHeaders)
         headers.append(contentsOf: config.extraHeaders)
