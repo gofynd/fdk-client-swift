7c0af42f01fa1a4ebe31da72f14c77919842a950
Jigar Dafda <jigar.dafda@gmail.com>
2025-02-13 07:05:58 +0000

    [Auto Generated] 3.0.0-beta.5

--- FDKClient.podspec
+++ FDKClient.podspec
@@ -5,7 +5,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'FDKClient'
-  s.version          = '3.0.0-beta.3'
+  s.version          = '3.0.0-beta.5'
   s.summary          = 'FDK Client SDK for Swift language'
 
   s.description      = 'FDK Client SDK for Swift language that can be used to make Apps or extensions.'
--- Sources/code/public/PublicAPIClient.swift
+++ Sources/code/public/PublicAPIClient.swift
@@ -9,7 +9,7 @@ class PublicAPIClient {
                         responseType: String = "application/json",
                         onResponse: @escaping OnResponse) {
         var headers = [
-            (key: "x-fp-sdk-version", value: "3.0.0-beta.3")
+            (key: "x-fp-sdk-version", value: "3.0.0-beta.5")
         ]
         headers.append(contentsOf: extraHeaders)
         headers.append(contentsOf: config.extraHeaders)
